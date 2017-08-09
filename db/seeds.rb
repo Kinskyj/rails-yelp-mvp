df6932afa2ffdb96084a3b8bea2feaad81c78e3d
Jan Kinsky <jan.a.kinsky@gmail.com>
2017-08-09 15:35:53 +0200

    yelp using rails, backend done, basic frontend done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "chinese",
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "italian",
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "french",
+  },
+   {
+    name:         "Obladi",
+    address:      "eeeeh ooooohhh",
+    category:     "french",
+  },
+   {
+    name:         "Oblada",
+    address:      "ooooh aaahhh",
+    category:     "italian",
+  },
+   {
+    name:         "Dadada",
+    address:      "lilalilamoum",
+    category:     "french",
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
